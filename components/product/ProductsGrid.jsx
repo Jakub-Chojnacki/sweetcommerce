b9c1802b502550c5019f26e3eb4c8c0ca76441b8
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-06-23 10:21:47 +0200

    Added a ProductsGrid component that shows 6 newest products

---
+++ components/product/ProductsGrid.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {urlFor } from '../../lib/client';
+import { SimpleGrid,Flex,Heading,Image,Text } from '@chakra-ui/react'
+import Link from 'next/link'
+const ProductsGrid = ({items}) => {
+  console.log(items)
+  return (
+    <Flex direction='column' my={4} gap={8}>
+        <Heading color='pink.400'>You may also like:</Heading>
+        <SimpleGrid columns={['1','2','4','6']} align='center' >
+           { items.map((item)=> {
+                return (
+                <Link key={item.slug.current} href={`/product/${item.slug.current}`}>
+                    <Flex cursor='pointer' direction='column' gap={2} align='center' justify='center'>
+                        <Image boxSize={24} src={urlFor(item.defaultProductVariant.images[0])}/>
+                        <Text fontSize={12} fontWeight='bold'>{item.title}</Text>
+                        <Text fontSize={12}>${item.defaultProductVariant.price}</Text>
+                    </Flex>
+                </Link>
+                )
+            })}
+        </SimpleGrid>
+    </Flex>
+  )
+}
+
+export default ProductsGrid
--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { client, urlFor } from '../../lib/client';
+import { client } from '../../lib/client';
 import {PortableText} from '@portabletext/react'
-import { SimpleGrid,VStack,Flex,Image,Heading,Text,Box,Button} from '@chakra-ui/react'
+import { SimpleGrid,VStack,Flex,Heading,Text,Button} from '@chakra-ui/react'
 import ProductTabs from '../../components/product/ProductTabs'
+import ProductsGrid from '../../components/product/ProductsGrid'
 import {AiOutlinePlus,AiOutlineMinus,AiOutlineShoppingCart} from 'react-icons/ai'
-const ProductDetails = ({ product, products,vendor }) => {
+const ProductDetails = ({ product,vendor,newestSix }) => {
   const {title,defaultProductVariant:{grams,price,images},tags,categories,body } = product;
-
   return (
     // <div>
     //     <img src={urlFor(images[0])}/>
@@ -17,31 +17,33 @@ const ProductDetails = ({ product, products,vendor }) => {
     //     <img src={urlFor(vendor[0].logo)}/>
     //   </div>
     // </div>
-
-<Flex p={12} direction={['column','column','row']} align='center' gap={{md:'12',lg:'16'}} marginY={{md:'6'}}>
-<ProductTabs images={images}/>
-  <VStack  marginBottom={'6'} marginTop={['6','6','0']} align='left'>
-     <Heading color='pink.400' textTransform='uppercase' size='sm'>{vendor[0].title}</Heading>
-     <Heading>{title}</Heading>
-     <Flex py={6} fontSize={14} color='gray.500'><PortableText fontSize={4} value={body?.en}/></Flex>
-     <Flex paddingY={{md:'6'}} color='neutral.dark'> 
-      <Flex align='center' gap={6}>
-        <Text fontSize='3xl' fontWeight='bold' color='pink.400'>${price}</Text>
-        <Text>/ {grams} grams</Text>
+<Flex direction="column">
+  <Flex p={12} direction={['column','column','row']} align='center' gap={{md:'12',lg:'16'}} marginY={{md:'6'}}>
+    <ProductTabs images={images}/>
+    <VStack  marginBottom={'6'} marginTop={['6','6','0']} align='left'>
+      <Heading color='pink.400' textTransform='uppercase' size='sm'>{vendor[0].title}</Heading>
+      <Heading>{title}</Heading>
+      <Flex py={6} fontSize={14} color='gray.500'><PortableText fontSize={4} value={body?.en}/></Flex>
+      <Flex paddingY={{md:'6'}} color='neutral.dark'> 
+        <Flex align='center' gap={6}>
+          <Text fontSize='3xl' fontWeight='bold' color='pink.400'>${price}</Text>
+          <Text>/ {grams} grams</Text>
+        </Flex>
       </Flex>
-    </Flex>
-    <SimpleGrid  columns={['1','1','2']} gap={['2','2','6']} >
-      <Flex  borderRadius='4px' background='neutral.light' p={4} align='center' justify='space-between'>
-        <AiOutlineMinus cursor='pointer'/>
-        <Text>3</Text>
-        <AiOutlinePlus  cursor='pointer'/>
-      </Flex>
-      <Button py={7} backgroundColor='pink.400' color='white' gap={4}>
-        <AiOutlineShoppingCart color='white'/>
-        Add to cart
-        </Button>
-    </SimpleGrid> 
-  </VStack>
+      <SimpleGrid  columns={['1','1','2']} gap={['2','2','6']} >
+        <Flex  borderRadius='4px' background='neutral.light' p={4} align='center' justify='space-between'>
+          <AiOutlineMinus cursor='pointer'/>
+          <Text>3</Text>
+          <AiOutlinePlus  cursor='pointer'/>
+        </Flex>
+        <Button py={7} backgroundColor='pink.400' color='white' gap={4}>
+          <AiOutlineShoppingCart color='white'/>
+          Add to cart
+          </Button>
+      </SimpleGrid> 
+    </VStack>
+  </Flex>
+  <ProductsGrid items={newestSix}/>
 </Flex>
   )
 }
@@ -73,14 +75,18 @@ export const getStaticProps = async ({ params: { slug }}) => {
  
   const productsQuery = '*[_type == "product"]'
 
+  const newestSixQuery = '*[_type == "product"] | order(_createdAt desc)[0..5]'
+
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
   const vendorQuery = `*[_type == "vendor" && _id=='${product.vendor._ref}']`
   const vendor = await client.fetch(vendorQuery)
+  const newestSix = await client.fetch(newestSixQuery)
+
 
 
   return {
-    props: { products, product,vendor}
+    props: { products, product,vendor,newestSix} 
   }
 }
 
